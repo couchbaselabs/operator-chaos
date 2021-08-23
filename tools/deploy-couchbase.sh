1bebce45fc5314221c141f8d0fbd7679f6e1752e
Patrick Stephens <patrick.stephens@couchbase.com>
2021-08-23 12:03:12 +0100

    Resolve wait and namespace issues

--- tools/deploy-couchbase.sh
+++ tools/deploy-couchbase.sh
@@ -22,7 +22,7 @@ SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:7.0.0}
 SERVER_COUNT=${SERVER_COUNT:-3}
 # The namespaces to use
 DAC_NAMESPACE=${DAC_NAMESPACE:-couchbase-dac}
-declare -a TEST_NAMESPACES=("couchbase-test1" "couchbase-test1" "couchbase-test1")
+declare -a TEST_NAMESPACES=("couchbase-test1" "couchbase-test2" "couchbase-test3")
 
 # Add Couchbase via helm chart (depending on whether you include the extra slash or not)
 helm repo add couchbase https://couchbase-partners.github.io/helm-charts/ || helm repo add couchbase https://couchbase-partners.github.io/helm-charts
@@ -41,5 +41,12 @@ for NAMESPACE in "${TEST_NAMESPACES[@]}"; do
         --set cluster.image="${SERVER_IMAGE}",cluster.servers.default.size="${SERVER_COUNT}",install.admissionController=false \
         --namespace "${NAMESPACE}" --create-namespace --wait
 
-    echo "Completed Couchbase deployment"
+    # Wait for deployment to complete, a call to --wait may work with helm but it can be flakey
+    echo "Waiting for Couchbase to start up..."
+    # The operator uses readiness gates to hold the containers until the cluster is actually ready to be used
+    until [[ $(kubectl get pods --namespace "${NAMESPACE}" --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
+        echo -n '.'
+        sleep 2
+    done
+    echo "Completed Couchbase deployment into: $NAMESPACE"
 done
